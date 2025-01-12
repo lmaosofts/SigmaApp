b38853680da038a9aea5307515c86ff1566c0b20
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-01-12 09:51:22 +0200

    set up

---
+++ userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         sigma app for bonkios
+// @namespace    http://tampermonkey.net/
+// @version      -1
+// @author       lmaosofts
+// @match        https://bonkiscoolsite.neocities.org/os/
+// @icon         https://www.example.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	
+})()
