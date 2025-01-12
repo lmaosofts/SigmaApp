d1c749c5967eb9714cbc0692a01c5a43a3e5659c
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-01-12 09:55:43 +0200

    zoinks

--- userscript.js
+++ userscript.js
@@ -9,5 +9,7 @@
 // ==/UserScript==
 
 (function() {
-	
+	if(document.querySelector("div#sigmaicon+div#apps"))return // do not add new sigma icon if already present
+	document.getElementById("apps").insertAdjacentHTML("beforeBegin", `<div style="margin-left: 5%; margin-top: 14%; position: absolute; justify-content: center; width: 80px; height: 110px;" id="sigmaicon"><div class="centerdiv"><img style="image-rendering: pixelated; margin-top: 0.3%; width: 64px;" src="https://lmaosofts.github.io/SigmaApp/sigma.png" alt=""></div><p class="icontext">sigma app</p></div>`)
+	var icon = document.getElementById("sigmaicon")
 })()
